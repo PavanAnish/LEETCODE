2c5f52010bdc35df95d30be0006dfa7b4cb1f5ff
PavanAnish <pavanpavan6167@gmail.com>
2025-08-03 17:40:27 +0530

    Updated

---
+++ 238. Product of Array Except Self.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int [] answer=new int [n];
+        Arrays.fill(answer,1);//Fills array with 1
+        int out=1;
+        for(int i=0;i<n;i++){
+            answer[i]*=out;
+            out*=nums[i];
+        }
+        out=1;
+        for(int i=n-1;i>=0;i--){
+            answer[i]*=out;
+            out*=nums[i];
+        }
+    return answer;
+    }
+
+}
--- 27. Remove Element.java
+++ 27. Remove Element.java
@@ -1,7 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int k=0;
-        int [] nums1 = new int [nums.length];
         for(int i =0;i<nums.length;i++){
             if(nums[i]!=val){
                 
